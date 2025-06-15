e5ec8821c1287e40a10c39014803875ee7154c9e
Apostlex0 <sachinnb999@gmail.com>
2025-06-15 17:53:39 +0530

    lexer initialised and basic syntax processing coded

---
+++ README.md
@@ -0,0 +1,1 @@
+A handwritten compiler implementation built from scratch in C#
--- mc/Program.cs
+++ mc/Program.cs
@@ -7,13 +7,124 @@ namespace Mc
         {
             while (true)
             {
-                Console.Writer("> ");
+                Console.Write("> ");
                 var line = Console.ReadLine();
                 if (string.IsNullOrEmpty(line))
                     return;
-                if (line =="1 + 2 * 3")
-                    Console.WriterLine("7");
+                
+                var lexer = new Lexer(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+                    if (token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+                    Console.Write($"{token.Kind}: '{token.Text}' (Position: {token.Position})");
+                    if (token.Value != null)
+                        Console.Write($" (Value: {token.Value})");
+                    
+                    Console.WriteLine();
+                }                
             }
+        }
+    }
+
+    enum SyntaxKind
+    {
+        NumberToken,
+        PlusToken,
+        MinusToken,
+        MultiplyToken,
+        DivideToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        WhitespaceToken,
+        BadToken,
+        EndOfFileToken
+    }
+
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text , object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+        public object Value { get; } // Can be int for numbers, null for operators and parentheses
+    }
+
+
+    class Lexer 
+    {
+        private readonly string _text;
+        private int _position;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+
+        private char Current
+        {
+            get 
+            {
+                if (_position >= _text.Length)
+                    return '\0'; // End of input
+                return _text[_position];
+            }
+        }
+        private void Next(){
+            _position++;
+        }
+
+        public SyntaxToken NextToken()
+        {
+            // <numbers>
+            // + - * / ()
+            // <whitespaces>
+            if (_position >= _text.Length)
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+
+            if (char.IsDigit(Current))
+            {
+                var start = _position;
+                while (char.IsDigit(Current))
+                    Next();
+                
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+            if (char.IsWhiteSpace(Current))
+            {
+                var start = _position;
+                while (char.IsWhiteSpace(Current))
+                    Next();
+                
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
+            }
+            if (Current == '+')
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null); 
+            if (Current == '-')
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            if (Current == '*')
+                return new SyntaxToken(SyntaxKind.MultiplyToken, _position++, "*", null);
+            if (Current == '/')
+                return new SyntaxToken(SyntaxKind.DivideToken, _position++, "/", null);
+            if (Current == '(')
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);  
+            if (Current == ')')
+                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+            
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
         }
     }
 }
