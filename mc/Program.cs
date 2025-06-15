aea7eb07162edc27d5f04d1f7bbe8a03d4c6d9a2
Apostlex0 <sachinnb999@gmail.com>
2025-06-15 16:59:35 +0530

    initial commit

---
+++ mc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Writer("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    return;
+                if (line =="1 + 2 * 3")
+                    Console.WriterLine("7");
+            }
+        }
+    }
+}
