99b22e62f064b78246e52e489fd6c639456bdbc3
Apostlex0 <sachinnb999@gmail.com>
2026-06-02 23:22:42 +0530

    some small text changes

--- Jokerlib/CodeAnalysis/Binding/BoundBinaryOperator.cs
+++ Jokerlib/CodeAnalysis/Binding/BoundBinaryOperator.cs
@@ -17,19 +17,19 @@ namespace Joker.CodeAnalysis.Binding
 
         }
 
-        private BoundBinaryOperator(SyntaxKind syntaxKind, BoundBinaryOperatorKind kind, Type leftType, Type rightTye, Type resultType)
+        private BoundBinaryOperator(SyntaxKind syntaxKind, BoundBinaryOperatorKind kind, Type leftType, Type rightType, Type resultType)
         {
             SyntaxKind = syntaxKind;
             Kind = kind;
             LeftType = leftType;
-            RightTye = rightTye;
+            RightType = rightType;
             Type = resultType;
         }
 
         public SyntaxKind SyntaxKind { get; }
         public BoundBinaryOperatorKind Kind { get; }
         public Type LeftType { get; }
-        public Type RightTye { get; }
+        public Type RightType { get; }
         public Type Type { get; }
 
         private static BoundBinaryOperator[] _operators =
@@ -52,7 +52,7 @@ namespace Joker.CodeAnalysis.Binding
         {
             foreach (var op in _operators)
             {
-                if (op.SyntaxKind == syntaxKind && op.LeftType == leftType && op.RightTye == rightType)
+                if (op.SyntaxKind == syntaxKind && op.LeftType == leftType && op.RightType == rightType)
                     return op;
             }
 
